95ef86d714c79ad30b1d9514cc634fb49cdaf9f1
knobico1337 <knobico@gmail.com>
2019-08-19 16:26:51 +0200

    Created user register, auth and login

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ const config = require('config')
 const db = config.get('mongoURI')
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        })
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
 
-        console.log('MongoDB Connected...')
-    } catch (err) {
-        console.error(err.message)
-        // Exit process with failure
-        process.exit(1)
-    }
+    console.log('MongoDB Connected...')
+  } catch (err) {
+    console.error(err.message)
+    // Exit process with failure
+    process.exit(1)
+  }
 }
 
 module.exports = connectDB
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,75 @@
 const express = require('express')
 const router = express.Router()
+const auth = require('../../middleware/auth')
+const jwt = require('jsonwebtoken')
+const { check, validationResult } = require('express-validator/check')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/User')
 
 // @route   GET api/auth
 // @desc    TEST route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'))
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password')
+    res.json(user)
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send('Server error')
+  }
+})
+
+// @route   POST api/auth
+// @desc    Authenticate user & get token
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists()
+  ],
+  async (req, res) => {
+    // use repo here because bad request register a user everytime
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    const { email, password } = req.body
+
+    try {
+      let user = await User.findOne({ email })
+      if (!user) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password)
+      if (!isMatch) {
+        return res.status(400).json({ errors: [{ msg: 'Invalid credentials' }] })
+      }
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      }
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        }
+      )
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send('Server error')
+    }
+  }
+)
 
 module.exports = router
